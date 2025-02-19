dae66f61ae8a9e7c41a1570e60073465da431468
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-02-19 10:55:58 -0300

    Add getImageUrl method to Pokemon data class for sprite retrieval

--- app/src/main/java/com/example/pokerecyclerview/Pokemon.kt
+++ app/src/main/java/com/example/pokerecyclerview/Pokemon.kt
@@ -3,7 +3,12 @@ package com.example.pokerecyclerview
 data class Pokemon(
     val name: String,
     val url: String
-)
+) {
+    fun getImageUrl(): String {
+        val id = url.split("/".toRegex()).dropLast(1).last()
+        return "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$id.png"
+    }
+}
 
 data class PokemonResponse(
     val results: List<Pokemon>
